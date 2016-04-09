10df42e0d3479c79375c4d963bce38c3fcb710bb
yoel <yoel12344@walla.co.il>
2016-04-09 23:54:32 +0300

    commit 2

---
+++ header.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once('yapi/yform/yform.php');
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php echo $title;?></title>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include "header.php";?>
+
+<?php 
+
+
+
+include('yapi/yform/example_usge.php');
+
+ ?>
+
+<?php include "footer.php";?>
